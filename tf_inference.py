f6079a4df90b83d4ac1026d743ac52d647af0838
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-05-15 12:42:17 +0300

    Fix tensorflow inference

--- tf_inference.py
+++ tf_inference.py
@@ -1,12 +1,20 @@
+import os
+from os import path
+
+import imageio
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 
 
 name = "train/tf.pb"
+images_dir = 'mnist-images'
+
+
+files = [path.join(images_dir, p) for p in os.listdir(images_dir)]
+
 
 with tf.Session() as persisted_sess:
-    print("load graph")
     with gfile.FastGFile(name, 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
@@ -14,11 +22,17 @@ with tf.Session() as persisted_sess:
     persisted_sess.graph.as_default()
     tf.import_graph_def(graph_def, name='')
 
-    test = np.random.rand(1, 1, 28, 28).astype(np.float32)
-
+    # test = np.random.rand(1, 1, 28, 28).astype(np.float32).argmax()
     inp = persisted_sess.graph.get_tensor_by_name('0:0')
     out = persisted_sess.graph.get_tensor_by_name('LogSoftmax:0')
-    feed_dict = {inp: test}
+    __import__('pdb').set_trace()
 
-    classification = persisted_sess.run(out, feed_dict)
+    for f_name in files:
+        data = imageio.imread(f_name).reshape(1, 1, 28, 28)
+        base_name = path.basename(f_name)
 
+        feed_dict = {inp: data}
+
+        classification = persisted_sess.run(out, feed_dict)
+
+        print('Prediction of file %s: %s' % (base_name, classification.argmax()))
