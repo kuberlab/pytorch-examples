307122c53b97126d0e8b902218ff3b4cf0ebf929
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2018-05-17 10:56:04 +0300

    Generalize pytorch -> onnx convert

--- mnist.py
+++ mnist.py
@@ -23,103 +23,6 @@ logging.root.setLevel(logging.INFO)
 LOG = logging.getLogger('main')
 
 
-# Training settings
-parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                    help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.01)')
-parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                    help='SGD momentum (default: 0.5)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument(
-    '--data-dir',
-    default=os.environ.get('DATA_DIR'),
-)
-parser.add_argument(
-    '--training_dir',
-    default=os.environ.get('TRAINING_DIR'),
-)
-parser.add_argument('--out-dir')
-parser.add_argument(
-    '--skip-mlboard',
-    action='store_true',
-    default=False,
-)
-args = parser.parse_args()
-use_cuda = not args.no_cuda and torch.cuda.is_available()
-use_mlboard = not args.skip_mlboard
-mlboard = client.Client()
-
-LOG.info('-' * 53)
-if use_cuda:
-    LOG.info('Use CUDA for processing.')
-else:
-    LOG.info('Do not use CUDA.')
-LOG.info('-' * 53)
-
-torch.manual_seed(args.seed)
-
-device = torch.device("cuda" if use_cuda else "cpu")
-
-
-kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-data_dir = args.data_dir
-training_dir = args.training_dir
-
-# If already processed
-if (not path.exists(path.join(training_dir, mnist.MNIST.processed_folder, mnist.MNIST.training_file)) or
-        not path.join(training_dir, mnist.MNIST.processed_folder, mnist.MNIST.test_file)):
-    # process and save as torch files
-    LOG.info('Processing dataset...')
-
-    files = os.listdir(data_dir)
-    for file in files:
-        full_path = path.join(data_dir, file)
-        save_path = path.join(training_dir, file.replace('.gz', ''))
-        with open(save_path, 'wb') as out_f, gzip.GzipFile(full_path) as zip_f:
-            out_f.write(zip_f.read())
-
-    training_set = (
-        mnist.read_image_file(path.join(training_dir, 'train-images-idx3-ubyte')),
-        mnist.read_label_file(path.join(training_dir, 'train-labels-idx1-ubyte'))
-    )
-    test_set = (
-        mnist.read_image_file(path.join(training_dir, 't10k-images-idx3-ubyte')),
-        mnist.read_label_file(path.join(training_dir, 't10k-labels-idx1-ubyte'))
-    )
-    os.makedirs(path.join(training_dir, mnist.MNIST.processed_folder))
-    with open(path.join(training_dir, mnist.MNIST.processed_folder, mnist.MNIST.training_file), 'wb') as f:
-        torch.save(training_set, f)
-    with open(path.join(training_dir, mnist.MNIST.processed_folder, mnist.MNIST.test_file), 'wb') as f:
-        torch.save(test_set, f)
-
-    LOG.info('Dataset processing done!')
-
-# Training dataset
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(root=training_dir, train=True, download=False,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])), batch_size=args.batch_size, shuffle=True, num_workers=4)
-# Test dataset
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST(root=training_dir, train=False, transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])), batch_size=args.batch_size, shuffle=True, num_workers=4)
-
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -137,11 +40,6 @@ class Net(nn.Module):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
-
-
-model = Net().to(device)
-
-optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
 
 def train(epoch):
@@ -184,24 +82,123 @@ def test():
         mlboard.update_task_info({'test_loss': test_loss, 'accuracy': accuracy / 100.})
 
 
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test()
-
-
 def save_checkpoint(state, filename):
     torch.save(state, filename)
 
 
-model_path = path.join(args.out_dir, 'checkpoint.pth.tar')
-LOG.info('Saving model to %s...' % model_path)
+if __name__ == '__main__':
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument(
+        '--data-dir',
+        default=os.environ.get('DATA_DIR'),
+    )
+    parser.add_argument(
+        '--training_dir',
+        default=os.environ.get('TRAINING_DIR'),
+    )
+    parser.add_argument('--out-dir')
+    parser.add_argument(
+        '--skip-mlboard',
+        action='store_true',
+        default=False,
+    )
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    use_mlboard = not args.skip_mlboard
+    mlboard = client.Client()
 
-if not path.exists(args.out_dir):
-    os.makedirs(args.out_dir)
+    LOG.info('-' * 53)
+    if use_cuda:
+        LOG.info('Use CUDA for processing.')
+    else:
+        LOG.info('Do not use CUDA.')
+    LOG.info('-' * 53)
 
-save_checkpoint(model, model_path)
+    torch.manual_seed(args.seed)
 
-if use_mlboard:
-    mlboard.update_task_info({'checkpoint_path': model_path})
+    device = torch.device("cuda" if use_cuda else "cpu")
 
-LOG.info('Saved.')
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    data_dir = args.data_dir
+    training_dir = args.training_dir
+
+    # If already processed
+    if (not path.exists(path.join(training_dir, mnist.MNIST.processed_folder, mnist.MNIST.training_file)) or
+            not path.join(training_dir, mnist.MNIST.processed_folder, mnist.MNIST.test_file)):
+        # process and save as torch files
+        LOG.info('Processing dataset...')
+
+        files = os.listdir(data_dir)
+        for file in files:
+            full_path = path.join(data_dir, file)
+            save_path = path.join(training_dir, file.replace('.gz', ''))
+            with open(save_path, 'wb') as out_f, gzip.GzipFile(full_path) as zip_f:
+                out_f.write(zip_f.read())
+
+        training_set = (
+            mnist.read_image_file(path.join(training_dir, 'train-images-idx3-ubyte')),
+            mnist.read_label_file(path.join(training_dir, 'train-labels-idx1-ubyte'))
+        )
+        test_set = (
+            mnist.read_image_file(path.join(training_dir, 't10k-images-idx3-ubyte')),
+            mnist.read_label_file(path.join(training_dir, 't10k-labels-idx1-ubyte'))
+        )
+        os.makedirs(path.join(training_dir, mnist.MNIST.processed_folder))
+        with open(path.join(training_dir, mnist.MNIST.processed_folder, mnist.MNIST.training_file), 'wb') as f:
+            torch.save(training_set, f)
+        with open(path.join(training_dir, mnist.MNIST.processed_folder, mnist.MNIST.test_file), 'wb') as f:
+            torch.save(test_set, f)
+
+        LOG.info('Dataset processing done!')
+
+    # Training dataset
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(root=training_dir, train=True, download=False,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])), batch_size=args.batch_size, shuffle=True, num_workers=4)
+    # Test dataset
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(root=training_dir, train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])), batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+    model = Net().to(device)
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        test()
+
+    model_path = path.join(args.out_dir, 'checkpoint.pth.tar')
+    LOG.info('Saving model to %s...' % model_path)
+
+    if not path.exists(args.out_dir):
+        os.makedirs(args.out_dir)
+
+    save_checkpoint(model, model_path)
+
+    if use_mlboard:
+        mlboard.update_task_info({'checkpoint_path': model_path})
+
+    LOG.info('Saved.')
--- torch2onnx.py
+++ torch2onnx.py
@@ -1,36 +1,51 @@
 from __future__ import print_function
 
-import torch.nn as nn
-import torch.nn.functional as F
+import argparse
+import importlib
+
 import torch.onnx
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--input',
+    required=True
+)
+parser.add_argument(
+    '--output',
+    required=True
+)
+parser.add_argument(
+    '--model-class',
+    help='path to model class. Example --model-class package1.module:ClassName',
+    required=True
+)
+parser.add_argument(
+    '--input-shape',
+    required=True,
+    nargs='+',
+    help='comma-separated tensor dimensions. Example: --input-shape 1,1,28,28'
+)
+args = parser.parse_args()
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+module = ':'.join(args.model_class.split(':')[:-1])
+class_name = args.model_class.split(':')[-1]
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
-
+module = importlib.import_module(module)
+clazz = getattr(module, class_name)
+globals()[class_name] = clazz
 
 print('loading torch model')
-model = torch.load('train/checkpoint.pth.tar')
-#model = Net()
-#model.load_state_dict(checkpoint)
-_input = torch.Tensor(1, 1, 28, 28)
+model = torch.load(args.input)
+
+print('generating input shapes...')
+inputs = ()
+for input_shape in args.input_shape:
+    shape = [int(i) for i in input_shape.split(',')]
+    print('add shape %s' % shape)
+    _input = torch.Tensor(*shape)
+    inputs += (_input,)
 
 print('exporting onnx model')
 # Export to ONNX
 model.train(False)
-torch_out = torch.onnx._export(model, _input, "train/model.onnx", export_params=True)
+torch_out = torch.onnx._export(model, inputs, args.output, export_params=True)
